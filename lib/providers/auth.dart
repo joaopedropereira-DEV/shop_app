d8987a12be511418ef49868d6085262ad6f2bf51
João Pedro <91147917+joaopedropereira-DEV@users.noreply.github.com>
2022-05-26 16:55:08 -0300

    Create AutoLogout

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
@@ -8,6 +9,7 @@ class Auth with ChangeNotifier {
   String? _email;
   String? _userId;
   DateTime? _expiryDate;
+  Timer? _logoutTimer;
 
   bool get isAuth {
     final isValid = _expiryDate?.isAfter(DateTime.now()) ?? false;
@@ -54,7 +56,7 @@ class Auth with ChangeNotifier {
           seconds: int.parse(body['expiresIn']),
         ),
       );
-
+      _autoLogout();
       notifyListeners();
     }
   }
@@ -72,7 +74,25 @@ class Auth with ChangeNotifier {
     _email = null;
     _userId = null;
     _expiryDate = null;
+    _clearLogoutTimer();
 
     notifyListeners();
+  }
+
+  void _clearLogoutTimer() {
+    _logoutTimer?.cancel();
+    _logoutTimer = null;
+  }
+
+  void _autoLogout() {
+    _clearLogoutTimer();
+
+    final timeToLogout = _expiryDate?.difference(DateTime.now()).inSeconds;
+    print(timeToLogout);
+
+    _logoutTimer = Timer(
+      Duration(seconds: timeToLogout ?? 0),
+      logout,
+    );
   }
 }
